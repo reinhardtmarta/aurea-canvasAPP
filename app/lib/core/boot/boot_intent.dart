bc6b40a0323b00b25eb146a26042f29699f19807
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-21 11:02:58 -0300

    Update boot_intent.dart

--- app/lib/core/boot/boot_intent.dart
+++ app/lib/core/boot/boot_intent.dart
@@ -1,1 +1,15 @@
+enum BootIntentType {
+  translate,
+  export,
+  analyze,
+}
 
+class BootIntent {
+  final BootIntentType type;
+  final String targetFormat;
+
+  BootIntent({
+    required this.type,
+    required this.targetFormat,
+  });
+}
