23d3d19e42f7d4cb971f2ca78e217afafcb92f1f
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-21 11:15:30 -0300

    Create boot_formats.dart

---
+++ app/lib/core/boot/boot_formats.dart
@@ -0,0 +1,9 @@
+const supportedFormats = {
+  'latex': ['latex', 'tex'],
+  'markdown': ['markdown', 'md'],
+  'text': ['txt', 'texto'],
+  'json': ['json'],
+  'png': ['png', 'imagem'],
+  'csv': ['csv'],
+  'code': ['codigo', 'code', 'python', 'dart', 'js'],
+};
